e73fadbd53b9ec2e2ae0336ee9043337104f2bea
Dean Iverson <diverson@vtti.vt.edu>
2019-12-31 20:22:32 -0700

    Converted constants from SCREAMING_CAPS to lowerCamelCase as per Dart language guidelines.

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -20,27 +20,35 @@ import 'package:spacefl/game/game_images.dart';
 import 'package:spacefl/game/game_state.dart';
 
 class Game {
-  static const bool PLAY_SOUND = true;
-  static const bool PLAY_MUSIC = true;
-  static const bool SHOW_BACKGROUND = true;
-  static const bool SHOW_STARS = true;
-  static const bool SHOW_ENEMIES = true;
-  static const bool SHOW_ASTEROIDS = true;
-  static const int NO_OF_STARS = SHOW_STARS ? 200 : 0;
-  static const int NO_OF_ASTEROIDS = SHOW_ASTEROIDS ? 15 : 0;
-  static const int NO_OF_ENEMIES = SHOW_ENEMIES ? 5 : 0;
+  static const bool playSound = true;
+  static const bool playMusic = true;
+  static const bool showStars = true;
+  static const bool showEnemies = true;
+  static const bool showAsteroids = true;
+  static const bool showBackground = true;
 
-  static const int LIVES = 5;
-  static const int SHIELDS = 10;
-  static const int DEFLECTOR_SHIELD_TIME = 5000;
-  static const int MAX_NO_OF_ROCKETS = 3;
-  static const double TORPEDO_SPEED = 6;
-  static const double ROCKET_SPEED = 4;
-  static const double ENEMY_TORPEDO_SPEED = 5;
-  static const double ENEMY_BOSS_TORPEDO_SPEED = 6;
-  static const int ENEMY_FIRE_SENSITIVITY = 10;
-  static const Duration ENEMY_BOSS_ATTACK_INTERVAL = Duration(seconds: 20);
-  static const Duration CRYSTAL_SPAWN_INTERVAL = Duration(seconds: 25);
+  static const int lifeCount = 5;
+  static const int shieldCount = 10;
+  static const int rocketCount = 3;
+  static const int starCount = showStars ? 200 : 0;
+  static const int asteroidCount = showAsteroids ? 15 : 0;
+  static const int enemyCount = showEnemies ? 5 : 0;
+  static const int enemyFireSensitivity = 10;
+
+  static const double torpedoSpeed = 6;
+  static const double rocketSpeed = 4;
+  static const double enemyTorpedoSpeed = 5;
+  static const double enemyBossTorpedoSpeed = 6;
+  static const double velocityFactorX = 0.5;
+  static const double velocityFactorY = 0.5;
+  static const double velocityFactorR = 0.01;
+
+  static const Duration deflectorShieldDuration = Duration(seconds: 5);
+  static const Duration enemyBossAttackInterval = Duration(seconds: 20);
+  static const Duration crystalSpawnInterval = Duration(seconds: 25);
+
+  static const Color scoreColor = Color.fromARGB(255, 51, 210, 206);
+
 //  static const double WIDTH = 700;
 //  static const double HEIGHT = 900;
 //  static const double FIRST_QUARTER_WIDTH = WIDTH * 0.25;
@@ -49,10 +57,6 @@ class Game {
 //  static const double SHIELD_INDICATOR_Y = HEIGHT * 0.06;
 //  static const double SHIELD_INDICATOR_WIDTH = WIDTH * 0.26;
 //  static const double SHIELD_INDICATOR_HEIGHT = HEIGHT * 0.01428571;
-  static const double VELOCITY_FACTOR_X = 0.5;
-  static const double VELOCITY_FACTOR_Y = 0.5;
-  static const double VELOCITY_FACTOR_R = 0.01;
-  static const Color SCORE_COLOR = Color.fromARGB(255, 51, 210, 206);
 
   static Game _instance;
 
@@ -61,6 +65,7 @@ class Game {
 
   Game._();
 
+  /// Return the singleton instance of Game
   factory Game.instance() {
     if (_instance == null) {
       _instance = Game._();
@@ -69,10 +74,7 @@ class Game {
     return Game._instance;
   }
 
-  Future<void> loadAssets() async {
-    final game = Game.instance();
-    await game.images.loadImages();
-  }
+  Future<void> loadAssets() async => await _instance.images.loadImages();
 
   void update(Duration deltaT) => _instance.state.update(this, deltaT);
 }
